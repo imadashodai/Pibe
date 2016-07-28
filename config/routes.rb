d552dd4e9a6bd8437f83291c1ac95165d292389c
shoday10 <imada@fiilse.com>
2016-07-28 11:40:19 +0000

    add check teams

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get 'join', to: 'teams#join'
+  post 'join', to: 'users#join'
   
   
   resources :users
