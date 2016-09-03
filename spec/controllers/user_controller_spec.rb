e587b3e9a957e542c15bcd9822fb99fcab302329
shoday10 <imada@fiilse.com>
2016-09-03 05:47:09 +0000

    modify controller test

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 require "rails_helper"
 
-#Getのテスト
 describe UsersController do
+    #showのテスト
     describe 'GET #show' do
         before do
             @user = FactoryGirl.create(:user)
@@ -12,12 +12,24 @@ describe UsersController do
             expect(assigns(:user)).to eq @user
         end
     end
-    
+    #createのテスト
     describe 'POST #create' do
         it 'create the user' do
             expect{
                 post :create, user: FactoryGirl.attributes_for(:user)
             }.to change(User, :count).by(1)
+        end
+    #validationエラーのテスト
+        it 'redirect to :new' do
+            post :create, user: FactoryGirl.attributes_for(:user, name: nil)
+            expect(response).to render_template :new
+        end
+        #updateのテスト
+        it 'change user' do
+            #@user = FactoryGirl.create(:user)
+            patch :update, id: @user, user: FactoryGirl.attributes_for(:user, name: "テスト")
+            user.reload
+            expect(user.name).to eq("テスト")
         end
     end
 end
