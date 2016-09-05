bfe3df1ad59d694c77d79dd75a042bda441e98f7
shoday10 <imada@fiilse.com>
2016-09-05 23:22:35 +0900

    modify meeting page

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,12 +1,18 @@
 class MyController < ApplicationController
     before_action :logged_in_user
-    
+
     def show
         @user = current_user
-        @team = @user.team
-        @team_users = @team.users
+		@team = @user.team
+
+		if @user.team != nil
+        	@team_users = @team.users
+		else
+			flash[:warning] = "チームを登録してください！"
+			redirect_to @user
+		end
     end
-    
+
     def edit
         @user = current_user
         @team = @user.team
