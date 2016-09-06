3a508baa0857421ad23da63d55b630e9526fc594
shoday10 <imada@fiilse.com>
2016-09-06 11:45:31 +0900

    create demo

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,6 @@
 class Meeting < ActiveRecord::Base
-  has_many :users
-  has_many :teams
+	validates :name, presence: true, length: { maximum: 20 }
+	validates :content, presence: true
+	has_many :users
+	has_many :teams
 end
